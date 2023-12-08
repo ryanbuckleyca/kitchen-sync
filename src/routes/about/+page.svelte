0708d4212909d0acde996df55c4b9ba2fe591da3
Ryan Buckley <ryanbuckley@gmail.com>
2023-12-08 12:10:12 -0500

    add routes

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About</h1>
+
+	<p>
+		311 is the color of our energy
+	</p>
+</div>
